79bd549b9081fd375f676f497377c7502df222f7
Stepan Zazuliak <61042159+StepanZazuliak@users.noreply.github.com>
2020-11-12 12:05:04 +0200

    Update TempSummaryStatistics.java

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -37,10 +37,9 @@ public class TempSummaryStatistics {
 
     public String toString() {
 
-        return "TempSummaryStatistics{" +
-                "avgTemp=" + avgTemp + ",
-                devTemp=" + devTemp + ",
-                minTemp=" + minTemp + ",
-                maxTemp=" + maxTemp + '}';
+        return "TempSummaryStatistics{" + "avgTemp=" + 
+            avgTemp + ", devTemp=" + devTemp + 
+            ", minTemp=" + minTemp + ", maxTemp=" + 
+            maxTemp + '}';
     }
 }
